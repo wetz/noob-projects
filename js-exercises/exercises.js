bcb6b334f599eed2edc8810e41b984108baff14b
Kristijan Banovec <bano89@gmail.com>
2017-02-02 21:26:50 +0100

    Fixed the FizzBuzz function.

--- js-exercises/exercises.js
+++ js-exercises/exercises.js
@@ -5,11 +5,13 @@ I will try to make this file as readable and easy to use as posible.
 By: Kristijan Banovec, https://github.com/wetz
 *****************************************************************************/
 // 1. FizzBuzz
-function FizzBuzz() {
+function FizzBuzz(n) {
+  for(i = 1; i <= n; i++){
      if(i % 15 == 0) console.log("FizzBuzz");
         else if(i % 5 == 0) console.log("Buzz");
         else if(i % 3 == 0) console.log("Fizz");
     else console.log(i);
+  }
 }
 // 2. Add the numbers from 1 to n together and return the result.
 function addNum(n){
